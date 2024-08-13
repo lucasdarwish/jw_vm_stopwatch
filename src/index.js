e9cc70d543ed82ac2f7c7864f1b36a3cdd385c4f
lucas.dias3 <lucas.dias@wedigi.com.br>
2024-08-13 17:07:58 -0300

    pwa-v2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { registerServiceWorker } from './service-worker'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,4 +18,4 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-serviceWorkerRegistration.register();
+registerServiceWorker();
